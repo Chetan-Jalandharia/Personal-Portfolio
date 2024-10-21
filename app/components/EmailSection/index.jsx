7941d4075b2b55baf882256cece7988134400146
Chetan Jalandharia <chetann8900@gmail.com>
2024-10-21 11:39:36 +0530

    Minor fix and Improvements

--- app/components/EmailSection/index.jsx
+++ app/components/EmailSection/index.jsx
@@ -38,15 +38,18 @@ const ContactSection = () => {
   };
 
   return (
-    <section id="contact" className="grid md:grid-cols-2 my-10 md:my-12 py-24 gap-4 relative">
-      {/* <div className="bg-[radial-gradient(ellipse_at_center,var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div> */}
-
+    <section
+      id="contact"
+      className="grid md:grid-cols-2 my-10 md:my-12 py-24 gap-4 relative"
+    >
       <div>
-        <h2 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h2>
+        <h2 className="text-xl font-bold text-white my-2">
+          Let&apos;s Connect
+        </h2>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I am currently looking for new oppertunities, my inbox is always open.
-          Weather you have a question or just want to say hi, I&apos;ll try my best
-          to get back to you!
+          I am currently looking for new opportunities, my inbox is always open.
+          Weather you have a question or just want to say hi, I&apos;ll try my
+          best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2 ">
           <Link href="https://github.com/Chetan-Jalandharia/">
